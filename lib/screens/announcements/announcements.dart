1c6970fe5b44a1777bcc0a9d23f56c7683e17073
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2024-07-25 22:37:10 +0800

    Update announcements.dart

---
+++ lib/screens/announcements/announcements.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Subjects_list extends StatefulWidget {
+  const Subjects_list({Key? key}) : super(key: key);
+  static String routeName = 'Subjects';
+
+  @override
+  _Subjects_listState createState() => _Subjects_listState();
+}
