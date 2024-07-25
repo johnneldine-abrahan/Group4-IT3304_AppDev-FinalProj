de8c974227fcceb70788f3cad17a6bd98efc9dd2
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2024-07-25 22:35:01 +0800

    Update subjects.dart

--- lib/screens/subjects/subjects.dart
+++ lib/screens/subjects/subjects.dart
@@ -7,3 +7,25 @@ class Subjects_list extends StatefulWidget {
   @override
   _Subjects_listState createState() => _Subjects_listState();
 }
+
+class _Subjects_listState extends State<Subjects_list> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = Tween<Offset>(begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0)).animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
